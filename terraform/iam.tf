e514b5509b74cdbffb02dd3150a7d38380e80262
Wes Henderson <whendersonii@gmail.com>
2026-05-03 01:52:59 -0500

    Moving iam to separate file.

---
+++ terraform/iam.tf
@@ -0,0 +1,109 @@
+### Load Balancers
+resource "aws_iam_policy" "lb_controller" {
+  name        = "AWSLoadBalancerControllerPolicy"
+  description = "Permissions for the AWS Load Balancer Controller."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeAddresses",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeAccountAttributes",
+          "ec2:DescribeInternetGateways",
+          "ec2:DescribeInstanceStatus",
+          "ec2:CreateTags",
+          "ec2:DeleteTags",
+          "ec2:ModifyNetworkInterfaceAttribute",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AttachNetworkInterface",
+          "ec2:DetachNetworkInterface",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupEgress"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+### Secrets
+resource "aws_iam_policy" "secrets_rw_policy" {
+  name        = "${var.control_plane_name}-rw-policy"
+  description = "Allow read/write access to Secrets Manager."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:CreateSecret",
+          "secretsmanager:UpdateSecret",
+          "secretsmanager:DeleteSecret",
+          "secretsmanager:ListSecrets"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+### Role
+resource "aws_iam_role" "cluster_role" {
+  name = "cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+### Attachments
+resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = aws_iam_policy.secrets_rw_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lb_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = aws_iam_policy.lb_controller.arn
+}
+
+### Profile
+resource "aws_iam_instance_profile" "cluster_profile" {
+  name = "${var.namespace}-profile"
+  role = aws_iam_role.cluster_role.name
+}
