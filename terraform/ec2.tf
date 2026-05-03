169b3e99f881161362929090ecf28e9114b7f516
Wes Henderson <whendersonii@gmail.com>
2026-05-03 01:54:56 -0500

    Passing news vars to user_data; exposing disk options.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,23 +1,36 @@
 ### Nodes
 resource "aws_instance" "control_plane" {
-  ami              = "${var.ami_id}"
-  instance_type    = "${var.control_plane_type}"
-  key_name         = "${var.key_name}"
-  user_data        = templatefile("scripts/bootstrap.sh", {
-    CONTROL_PLANE  = "true",
-    CP_SECRETS     = var.control_plane_secrets,
-    KUBE_VERSION   = var.kube_version,
-    CALICO_VERSION = var.calico_version,
-    POD_CIDR       = var.pod_cidr,
-    BASE_PACKAGES  = join(" ", var.base_packages),
-    KUBE_PACKAGES  = join(" ", var.kube_packages),
-    NAMESPACE      = var.namespace,
-    DEV_ROLE       = var.dev_team,
-    SUPPORT_ROLE   = var.support_team
+  ami                = "${var.ami_id}"
+  instance_type      = "${var.control_plane_type}"
+  key_name           = "${var.key_name}"
+  user_data          = templatefile("scripts/bootstrap.sh", {
+    REGION           = var.region,
+    VPC_ID           = var.vpc_id,
+    CONTROL_PLANE    = "true",
+    CP_SECRETS       = var.control_plane_secrets,
+    KUBE_VERSION     = var.kube_version,
+    CALICO_VERSION   = var.calico_version,
+    POD_CIDR         = var.pod_cidr,
+    BASE_PACKAGES    = join(" ", var.base_packages),
+    KUBE_PACKAGES    = join(" ", var.kube_packages),
+    NAMESPACE        = var.namespace,
+    DEV_ROLE         = var.rw_role,
+    SUPPORT_ROLE     = var.ro_role,
+    GATEWAY          = tostring(var.deploy_istio),
+    ISTIO_VERSION    = var.istio_version,
+    TELEPORT         = tostring(var.deploy_teleport),
+    TELEPORT_VERSION = var.teleport_version,
+    LB_CONTROLLER    = tostring(var.deploy_lb_controller),
+    ARGOCD           = tostring(var.deploy_argocd)
   })
 
   vpc_security_group_ids = [aws_security_group.control_plane_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.telekemp_instance_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.cluster_profile.name
+
+  root_block_device {
+    volume_size = var.control_plane_disk_size
+    volume_type = var.control_plane_disk_type
+  }
 
   tags = {
     Name = "${var.control_plane_name}"
@@ -26,29 +39,48 @@ resource "aws_instance" "control_plane" {
   depends_on = [
     aws_secretsmanager_secret.control_plane_secrets,
   ]
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
 }
 
 resource "aws_instance" "worker" {
-  ami              = "${var.ami_id}"
-  instance_type    = "${var.worker_type}"
-  key_name         = "${var.key_name}"
-  count            = var.worker_count
-  user_data        = templatefile("scripts/bootstrap.sh", {
-    CONTROL_PLANE  = "false",
-    CP_SECRETS     = var.control_plane_secrets,
-    KUBE_VERSION   = var.kube_version,
-    CALICO_VERSION = var.calico_version,
-    POD_CIDR       = var.pod_cidr,
-    BASE_PACKAGES  = join(" ", var.base_packages),
-    KUBE_PACKAGES  = join(" ", var.kube_packages),
-    NAMESPACE      = var.namespace,
-    DEV_ROLE       = var.dev_team,
-    SUPPORT_ROLE   = var.support_team
+  ami                = "${var.ami_id}"
+  instance_type      = "${var.worker_type}"
+  key_name           = "${var.key_name}"
+  count              = var.worker_count
+  user_data          = templatefile("scripts/bootstrap.sh", {
+    REGION           = var.region,
+    VPC_ID           = var.vpc_id,
+    CONTROL_PLANE    = "false",
+    CP_SECRETS       = var.control_plane_secrets,
+    KUBE_VERSION     = var.kube_version,
+    CALICO_VERSION   = var.calico_version,
+    POD_CIDR         = var.pod_cidr,
+    BASE_PACKAGES    = join(" ", var.base_packages),
+    KUBE_PACKAGES    = join(" ", var.kube_packages),
+    NAMESPACE        = var.namespace,
+    DEV_ROLE         = var.rw_role,
+    SUPPORT_ROLE     = var.ro_role,
+    GATEWAY          = tostring(var.deploy_istio),
+    ISTIO_VERSION    = var.istio_version,
+    TELEPORT         = tostring(var.deploy_teleport),
+    TELEPORT_VERSION = var.teleport_version,
+    LB_CONTROLLER    = tostring(var.deploy_lb_controller),
+    ARGOCD           = tostring(var.deploy_argocd)
   })
 
   #associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.worker_sg.id]
-  iam_instance_profile        = aws_iam_instance_profile.telekemp_instance_profile.name
+  iam_instance_profile        = aws_iam_instance_profile.cluster_profile.name
+
+  root_block_device {
+    volume_size = var.worker_disk_size
+    volume_type = var.worker_disk_type
+  }
 
   tags = {
     Name = "${var.worker_name}-${format("%02d", count.index + 1)}"
@@ -82,56 +114,4 @@ resource "aws_security_group" "worker_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-### IAM
-resource "aws_iam_role" "secrets_role" {
-  name = "${var.control_plane_name}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "secrets_rw_policy" {
-  name        = "${var.control_plane_name}-rw-policy"
-  description = "Allow read/write access to Secrets Manager."
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret",
-          "secretsmanager:PutSecretValue",
-          "secretsmanager:CreateSecret",
-          "secretsmanager:UpdateSecret",
-          "secretsmanager:DeleteSecret",
-          "secretsmanager:ListSecrets"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
-  role       = aws_iam_role.secrets_role.name
-  policy_arn = aws_iam_policy.secrets_rw_policy.arn
-}
-
-resource "aws_iam_instance_profile" "telekemp_instance_profile" {
-  name = "${var.control_plane_name}-profile"
-  role = aws_iam_role.secrets_role.name
 }
